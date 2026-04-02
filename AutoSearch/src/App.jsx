460d27119b5d1cc71e4518193f5a8d122f5465ee
Umair khan <ukkhan5476@gmail.com>
2026-04-02 18:23:12 +0530

    feat: implement caching for recipe search results

--- AutoSearch/src/App.jsx
+++ AutoSearch/src/App.jsx
@@ -4,13 +4,19 @@ const App = () => {
   const [recipes, setRecipes] = useState([]);
   const [input, setinput] = useState("");
   const [show, setShow] = useState(false);
+  const [cache, setCache] = useState({});
 
   const fetchData = async () => {
+    if (cache[input]) {
+      setRecipes(cache[input]);
+      return;
+    }
     const responese = await fetch(
       "https://dummyjson.com/recipes/search?q=" + input,
     );
     const data = await responese.json();
     setRecipes(data.recipes);
+    setCache((prev) => ({ ...prev, [input]: data?.recipes }));
   };
 
   useEffect(() => {
