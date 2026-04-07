5a48b2ba6d4c9db35f2ac20f59841993de04b2b7
Umair khan <ukkhan5476@gmail.com>
2026-04-08 01:47:36 +0530

    feat: implement routing with Layout, Body, About, and Contact components

--- FoodDelivery/src/main.jsx
+++ FoodDelivery/src/main.jsx
@@ -1,6 +1,34 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
+import About from "./pages/About.jsx";
+import Contact from "./pages/Cantact.jsx";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
+import Layout from "./layout/Layout.jsx";
+import Body from "./pages/Body.jsx";
 
-createRoot(document.getElementById("root")).render(<App />);
+const appRoute = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      },
+    ],
+    errorElement: <NotFoundPage />,
+  },
+]);
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={appRoute} />,
+);
