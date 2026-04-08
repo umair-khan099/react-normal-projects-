cfb19d0b21ca29bf21e4663d2c37412ab3ba15ba
Umair khan <ukkhan5476@gmail.com>
2026-04-09 00:44:02 +0530

    feat: add custom hook useSingleResturants for fetching restaurant data

---
+++ FoodDelivery/src/utils/useSingleResturants.js
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+
+const useSingleResturants = (resId) => {
+  const [collections, setCollections] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetchData();
+  }, [resId]);
+
+  const fetchData = async () => {
+    try {
+      const res = await fetch(
+        `https://www.eatsure.com/v1/api/get_all_products/brand_id/${resId}/store_id/10211/source_id/11`,
+      );
+      const json = await res.json();
+      setCollections(json?.data?.collections || []);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { collections, setLoading };
+};
+
+export default useSingleResturants;
