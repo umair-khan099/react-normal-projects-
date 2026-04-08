6db2d6b6528112c371a47c64cc64203db1f33754
Umair khan <ukkhan5476@gmail.com>
2026-04-09 01:52:45 +0530

    feat: refactor Body component to utilize useRestaurants hook for data management

--- FoodDelivery/src/pages/Body.jsx
+++ FoodDelivery/src/pages/Body.jsx
@@ -1,35 +1,11 @@
-import React, { useEffect, useState } from "react";
 import SearchBar from "../components/common/SearchBar";
 import OffersBanner from "../components/OffersBanner";
 import RestaurantsCard from "../components/RestaurantsCard";
-// import restorentData from "../../utils/RestoratnData";
+import useRestaurants from "../utils/useRestautants";
+
 const Body = () => {
-  const [data, setdata] = useState([]);
-  const [filteredData, setFilteredData] = useState([]);
-
-  const [search, setsearch] = useState("");
-  console.log(search);
-  const fetchData = async () => {
-    const res = await fetch("https://www.eatsure.com/v1/api/get_all_brands");
-    const json = await res.json();
-
-    setdata(json?.data?.data);
-    setFilteredData(json?.data?.data);
-  };
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const filterData = (search) => {
-    console.log(search);
-    let filtered = data.filter((items) =>
-      items.brand_name.toLowerCase().includes(search),
-    );
-    // setdata(filtered);
-    setFilteredData(filtered);
-  };
-
+  const { search, filteredData, data, setsearch, filterData } =
+    useRestaurants();
   return (
     <div>
       <SearchBar
